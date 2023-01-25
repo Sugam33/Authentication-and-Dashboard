66c959680ba485ee8ce2e593e56d0f50e2145564
Sugam Shakya <shakyasugam500@gmail.com>
2023-01-25 21:25:06 +0545

    mader slight changes to login and resgister section

--- src/auth/LoginReg.js
+++ src/auth/LoginReg.js
@@ -19,7 +19,6 @@ const TabPanel = (props) => {
 
 const LoginReg = () => {
     const [value, setValue] = useState(0);
-
     const handleChange = (event, newValue) => {
         setValue(newValue);
     }
