a47c22d97576f05b1b86405469060cac249f9906
Sugam Shakya <shakyasugam500@gmail.com>
2023-01-25 21:24:51 +0545

    made slight login and register section

--- src/auth/LoginReg.js
+++ src/auth/LoginReg.js
@@ -1,8 +1,55 @@
-import React from "react";
+import { Card, Grid, Tab, Tabs } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import pic1 from "../images/pic1.png"
+
+const TabPanel = (props) => {
+    const {children, value, index} = props;
+    console.log(props);
+    return(
+        <div role='tabpanel' hidden={value !== index}>
+            {
+                value === index && (
+                    <Box>{children}</Box>
+                )
+            }
+        </div>
+    )
+}
 
 const LoginReg = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
     return(
-        <h1>LoginReg</h1>
+        <div>
+            <Grid container sx={{height: '90vh'}}>
+                <Grid item lg={7} sm={5} sx={{
+                    backgroundImage:`url(${pic1})`,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center'
+                }}>
+                </Grid>
+
+                <Grid item lg={5} sm={7}>
+                    <Card sx={{width:'100%', height:'100%'}}>
+                        <Box>
+                            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                                <Tabs value={value} textColor="secondary" onChange={handleChange}>
+                                    <Tab label='Login' sx={{textTransform:'none', fontWeight:'bold', fontSize:'20px'}}></Tab>
+                                    <Tab label='Register' sx={{textTransform:'none', fontWeight:'bold', fontSize:'20px'}}></Tab>
+                                </Tabs>                                              
+                            </Box>
+                            <TabPanel value={value} index={0}>User Login</TabPanel>
+                            <TabPanel value={value} index={1}>Register</TabPanel>
+                        </Box>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
